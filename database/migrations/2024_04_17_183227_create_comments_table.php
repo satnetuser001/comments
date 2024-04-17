bc897617817953b5dd442066382a1b6f38a6e025
Anton Yurchenko <satnetuser001@gmail.com>
2024-04-17 18:50:31 +0300

    Database setup.
    Added database connection settings to the .env file.
    Created migrations according to the UML diagram.
    Applied the migrations.
    Dumped DB.

---
+++ database/migrations/2024_04_17_183227_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('user_name');
+            $table->string('home_page')->nullable();
+            $table->text('text');
+            $table->timestamps();
+
+            //table relation to itself
+            $table->foreign('parent_id')->references('id')->on('comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
--- database/migrations/2024_04_17_183248_create_files_table.php
+++ database/migrations/2024_04_17_183248_create_files_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('created_at')->nullable();
+            $table->foreignId('comment_id')->constrained()->onDelete('cascade');
+            $table->string('path');
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('files');
     }
 };
