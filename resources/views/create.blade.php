3f8136a792ae2fdb98f3124e866742c15121f51f
Anton Yurchenko <satnetuser001@gmail.com>
2024-05-04 22:22:49 +0300

    Added style for "Create Comment Page".

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,71 +2,79 @@
 @section('title', 'Create Comment Page')
 
 @section('content')
-    <div>
-        <a class="button" href="{{  route('home')  }}">Домой</a>
-    </div>
-
-    <form action="{{ route('store') }}" method="POST">
-        @csrf
-
-        <!-- User Name -->
-        <div class="">
-            <label><b>User Name</b></label>
-            <input name="userName" type="text" value="{{ old('userName') }}" class="">
-            <div class="errorMessage">
-                @error('userName')
-                    <span><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
-        </div>
-
-        <!-- E-mail -->
-        <div class="">
-            <label><b>Email</b></label>
-            <input name="email" type="text" value="{{ old('email') }}" class="">
-            <div class="errorMessage">
-                @error('email')
-                    <span><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
-        </div>
-
-        <!-- Home Page -->
-        <div class="">
-            <label><b>Home Page</b></label>
-            <input name="homePage" type="text" value="{{ old('homePage') }}" class="">
-            <div class="errorMessage">
-                @error('homePage')
-                    <span><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
-        </div>
-
-        <!-- CAPTCHA -->
+    <div class="createPage">
         <div>
-            <div>
-                <?php echo Captcha::img('flat'); ?>
-            </div>
-            <input name="captcha" type="text" autocomplete="off" class="">
-            <div class="errorMessage">
-                @error('captcha')
-                    <span><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
-            
+            <a class="button" href="{{  route('home')  }}">Домой</a>
         </div>
 
-        <!-- Text (Comment) -->
-        <div class="">
-            <label><b>Text</b></label>
-            <textarea name="text" class="">{{ old('text') }}</textarea>
-            <div class="errorMessage">
-                @error('text')
-                    <span ><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
-        </div>
+        <form class="createForm" action="{{ route('store') }}" method="POST">
+            @csrf
 
-        <input type="submit" class="" value="Добавить">
-    </form>
+            <div class="commentHead">
+                <div>
+                    Создать комментарий
+                </div>
+            </div>
+
+            <!-- User Name -->
+            <div class="input">
+                <label>Имя автора:</label>
+                <input name="userName" type="text" value="{{ old('userName') }}">
+                <div class="errorMessage">
+                    @error('userName')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- E-mail -->
+            <div class="input">
+                <label>Email:</label>
+                <input name="email" type="text" value="{{ old('email') }}">
+                <div class="errorMessage">
+                    @error('email')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- Home Page -->
+            <div class="input">
+                <label>Доманняя страница:</label>
+                <input name="homePage" type="text" value="{{ old('homePage') }}">
+                <div class="errorMessage">
+                    @error('homePage')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- CAPTCHA -->
+            <div class="input">
+                <label>Введите символы с картинки:</label>
+                <div>
+                    <?php echo Captcha::img('flat'); ?>
+                </div>
+                <input name="captcha" type="text" autocomplete="off">
+                <div class="errorMessage">
+                    @error('captcha')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- Text (Comment) -->
+            <div class="input">
+                <label>Комментарий:</label>
+                <textarea name="text">{{ old('text') }}</textarea>
+                <div class="errorMessage">
+                    @error('text')
+                        <span ><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <input type="submit" class="submit" value="Создать">
+        </form>
+    </div>
 @endsection
